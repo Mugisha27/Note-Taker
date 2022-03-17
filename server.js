86078a4021c29c011a08867f368321c557162b55
chris Mugisha <chrismugisha@chriss-MacBook-Air.local>
2022-03-16 23:26:07 -0700

    final commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require ('express');
 const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
+
 
 const app = express();
 
@@ -7,10 +10,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-app.use('api/notes', api);
+// app.use('api/notes', api);
 
 //set static folder//
 app.use(express.static(path.join(__dirname, 'public', 'index.html')));
+
+app.get('/notes', (req, res)=>{
+res.sendFile(path.join(__dirname, '/public', '/notes.html'))
+});
+
+app.get('/api/notes', (req, res)=>{
+res.json(db)
+});
+
+app.post('/api/notes', (req, res)=>{
+    const note=req.body
+    db.push(note)
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    res.json()
+});
 
 const PORT = process.env.PORT || 5003
  
